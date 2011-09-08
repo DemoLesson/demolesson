05bf98c3e0401193aff02a16c4308d95f221a78b
Andreas Lengyel <andreaslengyel@gmail.com>
2011-09-08 17:47:02 +0200

    committing dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
   layout 'standard'
     
   def index
+    if self.current_user.nil?
+      
+    elsif self.current_user.school != nil
+      puts "admin user"
+      @jobs = Job.find(:all, :conditions => ['school_id = ?', self.current_user.school.id], :order => 'created_at DESC')
+    elsif self.current_user.teacher != nil
+      @jobs = Job.find(:all, :limit => 4, :order => 'created_at DESC')
+    end
   end
-
+  
 end
