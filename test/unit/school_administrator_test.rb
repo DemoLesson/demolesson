d788a4ccd8cd36295f86c9bfdbe7ee28b7216583
Eric Scrivner <eric@grapheffect.com>
2012-08-03 22:15:02 -0700

    Fix Unit Tests
    - Repair failing fixtures and verify that unit tests pass.

--- test/unit/school_administrator_test.rb
+++ test/unit/school_administrator_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class SchoolAdministratorTest < ActiveSupport::TestCase
   test "should fail to save without user" do
-    sa = SchoolAdministrator.new(:school => schools(:denim_green).find)
+    sa = SchoolAdministrator.new(:school => schools(:denim_green))
     assert !sa.save, "Saved school administrator without a user"
+  end
+
+  test "should fail to save without school" do
+    sa = SchoolAdministrator.new(:user => users(:one))
+    assert !sa.save, "Saved school administrator without a school"
   end
 end
