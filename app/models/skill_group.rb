8512a98a8ed58facb8f8560887cf7602fb58f82d
Eric Scrivner <eric@grapheffect.com>
2012-08-04 23:03:21 -0700

    Add Correct Destroy Dependencies
    - All models for skills now have correct destruction rules.

--- app/models/skill_group.rb
+++ app/models/skill_group.rb
@@ -2,5 +2,5 @@ class SkillGroup < ActiveRecord::Base
   validates_presence_of :badge_url
   validates_presence_of :name
 
-  has_many :skills, :dependent => :destroy
+  has_many :skills, :dependent => :destroy_all
 end
