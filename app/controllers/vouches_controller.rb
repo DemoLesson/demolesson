c7e1db4d3a46c6204ab178d848252ba79e488642
Kelly Becker <kellylsbkr@gmail.com>
2012-09-01 15:52:11 -0700

    documented out vouches::unlocked

--- app/controllers/vouches_controller.rb
+++ app/controllers/vouches_controller.rb
@@ -69,62 +69,143 @@ class VouchesController < ApplicationController
   end
 
   def unlocked
+    # Get a user by looking for the email
     @user = User.find(:first, :conditions => ['email = ?', params[:user][:email]])
+
+    # If the user exists and GET ?urlstring does exist
     if @user != nil && params[:urlstring] != nil
+
+      # If the user has a teacher
       if @user.teacher != nil
-        @vouch=Vouch.find(:first, :conditions => ['url = ?', params[:urlstring]])
+
+        # Find the vouch matching the urlstring
+        @vouch = Vouch.find(:first, :conditions => ['url = ?', params[:urlstring]])
+
+        # For the the (new?) teacher loop through the skills
         @vouch.new_teacher_skills.each do |skill|
+
+          # Create a vouched skill for each skill in the loop and attach it to the user
           VouchedSkill.create(:user_id => @user.id, :skill_group_id => skill.skill_group_id)
         end
       end
+
+      # Redirect to Vouch Request
       redirect_to '/vouchrequest?u=' + params[:urlstring]
+
+    # If the user does exist and GET ?invitestring does exist
     elsif @user != nil && params[:invitestring] != nil
+
+      # If the user has a teacher
       if @user.teacher != nil
-        @invite=ConnectionInvite.find(:first, :conditions => ['url = ?', params[:invitestring]])
-        #if no longer pending simply don't create any connections
+
+        # Get the invite by the invitestring
+        @invite = ConnectionInvite.find(:first, :conditions => ['url = ?', params[:invitestring]])
+
+        # If no longer pending simply don't create any connections
         if @invite.pending
+
+          # Create two connections
           Connection.create(:owned_by => @user.id, :user_id => @invite.user_id, :pending => false)
           Connection.create(:owned_by => @invite.user_id, :user_id => @user.id, :pending => false)
+
+          # Create two activity logs
           Activity.create(:creator_id => @connection.user_id, :user_id => @connection.owned_by, :activityType => 10)
           Activity.create(:creator_id => @connection.owned_by, :user_id => @connection.user_id, :activityType => 10)
+
+          # Insert whiteboard log here
+          # Requires a logged in user though
+
+          # Set the invite to completed
           @invite.update_attribute(:pending, false)
+
+          # Redirect to the new users card url
           redirect_to '/card/'+ @user.teacher.url
         end
       end
+
+    # There is already an account with this email
+    # WAIT! If the above two conditions in this chain also require a user to exist.
+    # Possible security hole?
     elsif @user != nil
       redirect_to :back, :notice => "There is already an account with this email address."
+
+    # If no user exists
     else
-      password=User.random_string(10)
-      params[:user][:password]=password
-      params[:user][:confirm_password]=password
+      # Generate a random string (For a password)
+      password = User.random_string(10)
+      params[:user][:password] = password
+      params[:user][:confirm_password] = password
+
+      # Generate a new user with the random password
       @user = User.new(params[:user])
+
+      # If the user saved
       if @user.save
+
+        # Create a teacher on the user
         @user.create_teacher
+
+        # Send out the teacher welcome email with the temp password
         UserMailer.teacher_welcome_email_temppassword(@user.id, password).deliver
+
+        # Authenticate the user and set it to the session
         session[:user] = User.authenticate(@user.email, @user.password)
+
+        # If urlstring was provided
         if params[:urlstring]
+
+          # Fild a vouch matching urlstring
           @vouch=Vouch.find(:first, :conditions => ['url = ?', params[:urlstring]])
+
+          # Loop through the skills attached to the vouch
           @vouch.new_teacher_skills.each do |skill|
+
+            # Create a vouched skill on the user
             VouchedSkill.create(:user_id => @user.id, :skill_group_id => skill.skill_group_id)
           end
+
+          # Redirect to to vouch response?
           redirect_to '/vouchresponse?u=' + params[:urlstring]
+
+        # If invitestring was provided
         elsif params[:invitestring]
+
+          # Find the invite
           @invite=ConnectionInvite.find(:first, :conditions => ['url = ?', params[:invitestring]])
-          #if no longer pending simply don't create any connections
+
+          # If no longer pending simply don't create any connections
           if @invite.pending
+
+            # Create two connections
             Connection.create(:owned_by => @user.id, :user_id => @invite.user_id, :pending => false)
             Connection.create(:owned_by => @invite.user_id, :user_id => @user.id, :pending => false)
+
+            # Create two activities
             Activity.create(:creator_id => @invite.user_id, :user_id => @user.id, :activityType => 10)
             Activity.create(:creator_id => @user.id, :user_id => @invite.user_id, :activityType => 10)
+
+            # Put in the whiteboard insert here
+
+            # Set the invite to no longer pending
             @invite.update_attribute(:pending, false)
+
+            # Redirect to the users card profile
             redirect_to '/card/'+ @user.teacher.url
           end
+
+        # If user_connection is provided attemp to make the connection
         elsif params[:user_connection]
+
+          # Redirect to the connection handler
           redirect_to :controller => :connections, :action => :add_connection, :user_id => params[:user_connection]
         else
+
+          # Redirect to the card profile
           redirect_to '/card/'+ @user.teacher.url
         end
       else 
+        
+        # Redirect to the last visited page and show all the errors
         redirect_to :back, :notice => @user.errors.full_messages.to_sentence
       end
     end
