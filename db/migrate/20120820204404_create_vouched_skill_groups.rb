ed0f01d25e46aab1b995c66277787dedadbb5602
Elijah Green <elijahgreen@gmail.com>
2012-09-05 12:56:50 -0700

    Get back some migrations that were accidently removed

---
+++ db/migrate/20120820204404_create_vouched_skill_groups.rb
@@ -0,0 +1,11 @@
+class CreateVouchedSkillGroups < ActiveRecord::Migration
+  def change
+    create_table :vouched_skills do |t|
+      t.integer :skill_group_id
+      t.integer :user_id
+      t.integer :vouch_id
+
+      t.timestamps
+    end
+  end
+end
