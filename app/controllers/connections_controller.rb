e4f81ab8856e9b9eac2c078f85ae2f30d0863eb1
Elijah Green <elijahgreen@gmail.com>
2012-09-06 14:49:13 -0700

    Changed messages in connection invite emails.

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -152,7 +152,7 @@ class ConnectionsController < ApplicationController
 
   def inviteconnections
     @my_connection = Connection.find_for_user(self.current_user.id)
-    @default_message = "Hey! I'd love to add you to my professional teaching network at DemoLesson."
+    @default_message = "Hey, I just joined DemoLesson, and this is crazy, so here's my profile; add me maybe?"
   end
 
   def inviteconnection
@@ -209,7 +209,7 @@ class ConnectionsController < ApplicationController
             url = "http://#{request.host_with_port}/card?i=" + @invite.url
 
             # Send out the email
-            mail = UserMailer.connection_invite(self.current_user.name, email, url, params[:message]).deliver
+            mail = UserMailer.connection_invite(self.current_user, email, url, params[:message]).deliver
 
             # Notify the current session member that ht e email was sent
             notice << "Your invite to " + demail + " has been sent."
